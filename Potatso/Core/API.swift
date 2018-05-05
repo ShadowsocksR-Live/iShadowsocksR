307562bac886c5c673a02838c6142a2ec34dee89
ssrlive <ssrlivebox@gmail.com>
2018-05-05 23:45:16 +0800

    make swift 4.x happy.

--- Potatso/Core/API.swift
+++ Potatso/Core/API.swift
@@ -62,7 +62,7 @@ extension ProxyRuleSet: Mappable {
             return
         }
         var rules: [Rule] = []
-        if let parsedObject = Mapper<Rule>().mapArray(JSONArray: rulesJSON as! [[String : Any]]){
+        if let parsedObject = Mapper<Rule>().mapArray(JSONObject: rulesJSON){
             rules.append(contentsOf: parsedObject)
         }
         self.rules = rules
@@ -252,7 +252,7 @@ extension Alamofire.DataRequest {
             }
 
             if (JSONToMap != nil) {
-                if let parsedObject = Mapper<T>().mapArray(JSONArray: JSONToMap as! [[String : Any]]){
+                if let parsedObject = Mapper<T>().mapArray(JSONObject: JSONToMap){
                     return .success(parsedObject)
                 }
             }
