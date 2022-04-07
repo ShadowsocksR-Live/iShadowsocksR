7daffde822bc9907e641cb8d897d01c1b4607772
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-04-07 21:19:27 +0800

    weird hardcode for group ID.

--- PotatsoBase/Potatso.m
+++ PotatsoBase/Potatso.m
@@ -10,10 +10,19 @@
 @implementation Potatso
 
 + (NSString *) sharedGroupIdentifier {
+#if 1
     // reverted back
     // very curious why grabbing value from Info.plist will result in cannot connect to VPN all the time
     // it needs to be fixed like this as always
     return @"group.com.ssrlive.issr";
+#else
+    // FIXME: following code NOT working.
+    // Try to avoid hardcoding Group IDs into the source code.
+    // This is a fragile implementation that can be easily broken.
+    NSString *appID = [[NSBundle mainBundle] bundleIdentifier];
+    NSString *groupID = [@"group." stringByAppendingString:appID];
+    return groupID;
+#endif
 }
 
 + (NSURL *)sharedUrl {
