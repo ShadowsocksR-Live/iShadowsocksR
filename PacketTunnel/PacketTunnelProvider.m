f2dda3404a1b8cfdbc268246c38301b9e32617e6
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-06-14 13:43:50 +0800

    serverConnectivity timeout to 10 second

--- PacketTunnel/PacketTunnelProvider.m
+++ PacketTunnel/PacketTunnelProvider.m
@@ -66,7 +66,7 @@
         return;
     }
     
-    if (serverConnectivity(profile.server.UTF8String, (int)profile.serverPort, 3000) != 0){
+    if (serverConnectivity(profile.server.UTF8String, (int)profile.serverPort, 10000) != 0){
         completionHandler([NSError errorWithDomain:@"iShadowsocksR" code:-1 userInfo:@{NSLocalizedDescriptionKey:@"serverConnectivity"}]);
         return;
     }
